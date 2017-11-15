0a39270f38d008477e64de5eb9c2caacfacd5aed
jade-yu <jade_yu@dlsu.edu.ph>
2017-11-15 12:19:04 +0800

    added initializeTabs, renamed attributes for consistency
    initialization is still not working as of the moment, need to change to TabLayout instead of TabHost

--- app/src/main/java/edu/dlsu/mobapde/jam/Activities/MainActivity.java
+++ app/src/main/java/edu/dlsu/mobapde/jam/Activities/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.dlsu.mobapde.jam.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,84 +8,131 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 import edu.dlsu.mobapde.jam.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText et_search;
-    ImageView iv_search;
-    RelativeLayout footer;
-    ImageView main_album;
-    TextView tv_mainsong;
-    TextView tv_mainartist;
-    ImageButton ib_back;
-    ImageButton ib_play;
-    ImageButton ib_next;
+    EditText etSearch;
+    ImageView ivSearch;
+
+    TabHost tabHost;
+    TabHost.TabSpec tracksTab, artistsTab, albumsTab, playlistsTab, favesTab;
+
+    RelativeLayout rlFooter;
+    ImageView ivMainAlbum;
+    TextView tvMainsong, tvMainartist;
+    ImageButton ibBack, ibPlay, ibNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et_search = findViewById(R.id.et_search);
-        iv_search = findViewById(R.id.iv_search);
-        footer = findViewById(R.id.footer);
-        main_album = findViewById(R.id.main_album);
-        tv_mainsong = findViewById(R.id.tv_mainsong);
-        tv_mainartist = findViewById(R.id.tv_mainartist);
-        ib_back = findViewById(R.id.ib_back);
-        ib_play = findViewById(R.id.ib_play);
-        ib_next = findViewById(R.id.ib_next);
+        etSearch = findViewById(R.id.et_search);
+        ivSearch = findViewById(R.id.iv_search);
 
-        iv_search.setOnClickListener(new View.OnClickListener() {
+        rlFooter = findViewById(R.id.footer);
+        ivMainAlbum = findViewById(R.id.main_album);
+        tvMainsong = findViewById(R.id.tv_mainsong);
+        tvMainartist = findViewById(R.id.tv_mainartist);
+        ibBack = findViewById(R.id.ib_back);
+        ibPlay = findViewById(R.id.ib_play);
+        ibNext = findViewById(R.id.ib_next);
+
+        initializeTabs();
+
+        ivSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //todo put functions
             }
         });
 
-        main_album.setOnClickListener(new View.OnClickListener() {
+        ivMainAlbum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //todo put functions
             }
         });
 
-        tv_mainsong.setOnClickListener(new View.OnClickListener() {
+        tvMainsong.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //todo put functions
             }
         });
 
-        tv_mainartist.setOnClickListener(new View.OnClickListener() {
+        tvMainartist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //todo put functions
             }
         });
 
-        ib_back.setOnClickListener(new View.OnClickListener() {
+        ibBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //todo put functions
             }
         });
 
-        ib_play.setOnClickListener(new View.OnClickListener() {
+        ibPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //todo put functions
             }
         });
 
-        ib_next.setOnClickListener(new View.OnClickListener() {
+        ibNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //todo put functions
             }
         });
+    }
+
+    //TODO fix initialization, change to TabLayout
+    public void initializeTabs() {
+
+        Intent tracksIntent, artistsIntent, albumsIntent, playlistsIntent, favesIntent;
+
+        tabHost = (TabHost) findViewById(android.R.id.tabhost);
+        tabHost.setup();
+
+        tracksTab = tabHost.newTabSpec("tracks");
+        tracksTab.setIndicator("Tracks");
+        tracksIntent = new Intent(this, TracksActivity.class);
+        tracksTab.setContent(tracksIntent);
+        tabHost.addTab(tracksTab);
+
+        artistsTab = tabHost.newTabSpec("artists");
+        artistsTab.setIndicator("Artists");
+        artistsIntent = new Intent(this, ArtistsActivity.class);
+        artistsTab.setContent(artistsIntent);
+        
+        albumsTab = tabHost.newTabSpec("albums");
+        albumsTab.setIndicator("Albums");
+        albumsIntent = new Intent(this, AlbumsActivity.class);
+        albumsTab.setContent(albumsIntent);
+        
+        playlistsTab = tabHost.newTabSpec("playlists");
+        playlistsTab.setIndicator("Playlists");
+        //TODO make playlist activity
+//        playlistsIntent = new Intent(this, PlaylistsActivity.class);
+//        playlistsTab.setContent(playlistsIntent);
+
+        //TODO make faves activity
+        favesTab = tabHost.newTabSpec("faves");
+        favesTab.setIndicator("♥");
+//        favesIntent = new Intent(this, FavesActivity.class);
+//        favesTab.setContent(favesIntent);
+
+        tabHost.addTab(artistsTab);
+        tabHost.addTab(albumsTab);
+        tabHost.addTab(playlistsTab);
+        tabHost.addTab(favesTab);
     }
 }
