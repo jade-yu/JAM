4096241f409592a618b881a020ca12e89a4bc482
jade-yu <jade_yu@dlsu.edu.ph>
2017-11-15 13:09:30 +0800

    fixed getTrackList to check/ask for read permission

--- app/src/main/java/edu/dlsu/mobapde/jam/Activities/TracksActivity.java
+++ app/src/main/java/edu/dlsu/mobapde/jam/Activities/TracksActivity.java
@@ -1,12 +1,18 @@
 package edu.dlsu.mobapde.jam.Activities;
 
+import android.Manifest;
 import android.content.ContentResolver;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -29,6 +35,8 @@ public class TracksActivity extends AppCompatActivity {
 
         ArrayList<Track> trackList = getTrackList();
 
+        Log.d("tracksize", trackList.size() + "");
+
         TrackAdapter trackAdapter = new TrackAdapter(trackList);
         rvTracks.setAdapter(trackAdapter);
 
@@ -36,7 +44,7 @@ public class TracksActivity extends AppCompatActivity {
             @Override
             public void onItemClick(Track t) {
                 //TODO onItemClick implementation
-                System.out.println(t.getTitle());
+                Log.d("clickedtitle", t.getTitle());
             }
         });
 
@@ -45,33 +53,46 @@ public class TracksActivity extends AppCompatActivity {
 
     // Gets the list of songs from the device storage
     public ArrayList<Track> getTrackList() {
-        ContentResolver musicResolver = getContentResolver();
-        Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
 
         ArrayList<Track> trackList = new ArrayList<>();
 
-        if(musicCursor != null && musicCursor.moveToFirst()){
-            //get columns
-            int titleColumn = musicCursor.getColumnIndex(android.provider.MediaStore.Audio.Media.TITLE);
-            int idColumn = musicCursor.getColumnIndex(android.provider.MediaStore.Audio.Media._ID);
-            int artistColumn = musicCursor.getColumnIndex(android.provider.MediaStore.Audio.Media.ARTIST);
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
 
-            //add songs to list
-            do {
-                long id = musicCursor.getLong(idColumn);
-                String title = musicCursor.getString(titleColumn);
-                String artist = musicCursor.getString(artistColumn);
-
-                trackList.add(new Track(id, title, artist));
-            } while (musicCursor.moveToNext());
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
         }
 
-        Collections.sort(trackList, new Comparator<Track>(){
-            public int compare(Track a, Track b){
-                return a.getTitle().compareTo(b.getTitle());
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            ContentResolver musicResolver = getContentResolver();
+            Uri musicUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+            Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+
+            Log.d("cursorsize", musicCursor.getCount() + "");
+
+            if(musicCursor != null && musicCursor.moveToFirst()){
+                //get columns
+                int titleColumn = musicCursor.getColumnIndex(android.provider.MediaStore.Audio.Media.TITLE);
+                int idColumn = musicCursor.getColumnIndex(android.provider.MediaStore.Audio.Media._ID);
+                int artistColumn = musicCursor.getColumnIndex(android.provider.MediaStore.Audio.Media.ARTIST);
+
+                //add songs to list
+                do {
+                    long id = musicCursor.getLong(idColumn);
+                    String title = musicCursor.getString(titleColumn);
+                    String artist = musicCursor.getString(artistColumn);
+
+//                    Log.d("title", title);
+
+                    trackList.add(new Track(id, title, artist));
+                } while (musicCursor.moveToNext());
             }
-        });
+
+            Collections.sort(trackList, new Comparator<Track>(){
+                public int compare(Track a, Track b){
+                    return a.getTitle().compareTo(b.getTitle());
+                }
+            });
+        }
 
         return trackList;
     }
