4028f4f0ed136b824f213b1d2157751ff6369146
Julius Danielle Lola <julius_lola@dlsu.edu.ph>
2017-11-15 15:24:57 +0800

    FindViewByID
    puro find view by id lang boiis

--- app/src/main/java/edu/dlsu/mobapde/jam/Activities/SearchActivity.java
+++ app/src/main/java/edu/dlsu/mobapde/jam/Activities/SearchActivity.java
@@ -1,15 +1,95 @@
 package edu.dlsu.mobapde.jam.Activities;
 
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import edu.dlsu.mobapde.jam.R;
 
 public class SearchActivity extends AppCompatActivity {
 
+    //todo tabs
+
+    EditText etSearch;
+    ImageView ivSearch;
+    TabLayout tabLayout;
+    FrameLayout tabContent;
+    LinearLayout footer;
+    ImageView ivMainAlbum;
+    TextView tvMainsong, tvMainartist;
+    ImageButton ibBack, ibPlay, ibNext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        etSearch = findViewById(R.id.et_search);
+        ivSearch = findViewById(R.id.iv_search);
+        tabLayout = findViewById(R.id.tablayout);
+        tabContent = findViewById(R.id.tabcontent);
+        footer = findViewById(R.id.footer);
+        ivMainAlbum = findViewById(R.id.main_album);
+        tvMainsong = findViewById(R.id.tv_mainsong);
+        tvMainartist = findViewById(R.id.tv_mainartist);
+        ibBack = findViewById(R.id.ib_back);
+        ibPlay = findViewById(R.id.ib_play);
+        ibNext = findViewById(R.id.ib_next);
+
+        ivSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo put functions
+            }
+        });
+
+        ivMainAlbum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo put functions
+            }
+        });
+
+        tvMainsong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo put functions
+            }
+        });
+
+        tvMainartist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo put functions
+            }
+        });
+
+        ibBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo put functions
+            }
+        });
+
+        ibPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo put functions
+            }
+        });
+
+        ibNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo put functions
+            }
+        });
     }
 }
--- app/src/main/java/edu/dlsu/mobapde/jam/Activities/SearchLyricsActivity.java
+++ app/src/main/java/edu/dlsu/mobapde/jam/Activities/SearchLyricsActivity.java
@@ -2,14 +2,19 @@ package edu.dlsu.mobapde.jam.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import edu.dlsu.mobapde.jam.R;
 
 public class SearchLyricsActivity extends AppCompatActivity {
 
+    WebView wvSearch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_lyrics);
+
+        wvSearch = findViewById(R.id.wv_search);
     }
 }
