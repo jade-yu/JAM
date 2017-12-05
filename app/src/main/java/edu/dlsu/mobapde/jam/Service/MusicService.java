5eb2f57712a5b95cc5ae282b0134e34ec6c7f606
jade-yu <jade_yu@dlsu.edu.ph>
2017-12-05 23:36:31 +0800

    added player notification

--- app/src/main/java/edu/dlsu/mobapde/jam/Service/MusicService.java
+++ app/src/main/java/edu/dlsu/mobapde/jam/Service/MusicService.java
@@ -10,9 +10,14 @@ import android.os.Binder;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.util.Log;
+import java.util.Random;
+import android.app.Notification;
+import android.app.PendingIntent;
 
 import java.util.ArrayList;
 
+import edu.dlsu.mobapde.jam.Activities.PlaySongActivity;
+import edu.dlsu.mobapde.jam.R;
 import edu.dlsu.mobapde.jam.RecyclerViewItems.Track;
 
 public class MusicService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, MediaPlayer.OnCompletionListener {
@@ -20,6 +25,7 @@ public class MusicService extends Service implements MediaPlayer.OnPreparedListe
     private MediaPlayer player;
     private ArrayList<Track> tracks;
     private int currentPosition;
+    private static final int NOTIFY_ID = 1;
 
     private final IBinder musicBind = new MusicBinder();
 
@@ -27,7 +33,17 @@ public class MusicService extends Service implements MediaPlayer.OnPreparedListe
         super.onCreate();
 
         player = new MediaPlayer();
+        tracks = new ArrayList<>();
+
         initializeMusicPlayer();
+    }
+
+    public ArrayList<Track> getTrackList() {
+        return tracks;
+    }
+
+    public Track getCurrentTrack() {
+        return tracks.get(currentPosition);
     }
 
     public void setTracks(ArrayList<Track> tracks) {
@@ -54,12 +70,13 @@ public class MusicService extends Service implements MediaPlayer.OnPreparedListe
     }
 
     public void playTrack() {
-        player.reset();
+        //player.reset();
 
         Track track = tracks.get(currentPosition);
         Uri trackUri = ContentUris.withAppendedId(android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, track.getId());
 
         try {
+            player.reset();
             player.setDataSource(getApplicationContext(), trackUri);
             Log.d("playTrack: ", track.getTitle());
         } catch(Exception e){
@@ -69,6 +86,34 @@ public class MusicService extends Service implements MediaPlayer.OnPreparedListe
         player.prepareAsync();
     }
 
+    public void togglePlay() {
+        if(player.isPlaying()) {
+            player.pause();
+        } else {
+            player.start();
+        }
+    }
+
+    public void previousTrack() {
+        if(currentPosition == 0) {
+            currentPosition = tracks.size() - 1;
+        } else {
+            currentPosition--;
+        }
+
+        playTrack();
+    }
+
+    public void nextTrack() {
+        if(currentPosition == tracks.size() - 1) {
+            currentPosition = 0;
+        } else {
+            currentPosition++;
+        }
+
+        playTrack();
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
         Log.d("MusicService", "onBind");
@@ -76,16 +121,18 @@ public class MusicService extends Service implements MediaPlayer.OnPreparedListe
     }
 
     @Override
-    public boolean onUnbind(Intent intent){
+    public boolean onUnbind(Intent intent) {
         player.stop();
         player.release();
+        Log.d("MusicService", "onUnbind");
         return false;
     }
 
     //TODO Listener methods
     @Override
     public void onCompletion(MediaPlayer mediaPlayer) {
-
+        currentPosition++;
+        playTrack();
     }
 
     @Override
@@ -96,5 +143,26 @@ public class MusicService extends Service implements MediaPlayer.OnPreparedListe
     @Override
     public void onPrepared(MediaPlayer mp) {
         mp.start();
+
+        Intent notIntent = new Intent(this, PlaySongActivity.class);
+        notIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendInt = PendingIntent.getActivity(this, 0,
+                notIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification.Builder builder = new Notification.Builder(this);
+
+        builder.setContentIntent(pendInt)
+                .setSmallIcon(R.drawable.btn_play)
+                .setTicker(getCurrentTrack().getTitle())
+                .setOngoing(true)
+                .setContentTitle("Playing").setContentText(getCurrentTrack().getTitle());
+        Notification not = builder.build();
+
+        startForeground(NOTIFY_ID, not);
+    }
+
+    @Override
+    public void onDestroy() {
+        stopForeground(true);
     }
 }
