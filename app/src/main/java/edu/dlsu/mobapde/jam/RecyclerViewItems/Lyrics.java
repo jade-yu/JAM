39275bd440c49a7c3ef31553f21bf854fe36c264
Julius Danielle Lola <julius_lola@dlsu.edu.ph>
2017-12-12 12:34:31 +0800

    Added Lyrics
    lyricsAdapter, item_lyrics, lyrics class(not sure if kailangan pa to)

---
+++ app/src/main/java/edu/dlsu/mobapde/jam/RecyclerViewItems/Lyrics.java
@@ -0,0 +1,24 @@
+package edu.dlsu.mobapde.jam.RecyclerViewItems;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Asus on 08/12/2017.
+ */
+
+public class Lyrics {
+    private String wholeLyrics;
+
+    public Lyrics(String wholeLyrics) {
+        this.wholeLyrics = wholeLyrics;
+    }
+
+    public String getWholeLyrics() {
+        return wholeLyrics;
+    }
+
+    public void setWholeLyrics(String wholeLyrics) {
+        this.wholeLyrics = wholeLyrics;
+    }
+
+}
---
+++ app/src/main/java/edu/dlsu/mobapde/jam/RecyclerViewItems/LyricsAdapter.java
@@ -0,0 +1,60 @@
+package edu.dlsu.mobapde.jam.RecyclerViewItems;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import edu.dlsu.mobapde.jam.R;
+
+/**
+ * Created by Asus on 08/12/2017.
+ */
+
+public class LyricsAdapter extends RecyclerView.Adapter<LyricsAdapter.LyricsViewHolder> {
+
+    ArrayList<String> lineLyrics = new ArrayList<>();
+
+    public LyricsAdapter(String lyrics) {
+        toArrayList(lyrics);
+
+    }
+
+    public void toArrayList(String s) {
+        
+    }
+
+    @Override
+    public LyricsAdapter.LyricsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_lyrics, parent, false);
+        return new LyricsViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(LyricsAdapter.LyricsViewHolder holder, int position) {
+
+        String currentLyrics = lineLyrics.get(position);
+
+        holder.tvLyrics.setText(currentLyrics);
+        holder.itemView.setTag(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class LyricsViewHolder extends RecyclerView.ViewHolder {
+        TextView tvLyrics;
+
+        public LyricsViewHolder(View itemView) {
+            super(itemView);
+            tvLyrics = itemView.findViewById(R.id.tv_lyrics);
+
+        }
+    }
+}
