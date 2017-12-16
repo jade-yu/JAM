f883cab5479860d18bc9f83c6d032cbab8d2dd32
isakyaki <pamela_ortiz@dlsu.edu.ph>
2017-12-17 02:44:23 +0800

    editing the size of each lyric line

--- app/src/main/java/edu/dlsu/mobapde/jam/Activities/SearchLyricsActivity.java
+++ app/src/main/java/edu/dlsu/mobapde/jam/Activities/SearchLyricsActivity.java
@@ -3,6 +3,7 @@ package edu.dlsu.mobapde.jam.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import edu.dlsu.mobapde.jam.R;
 
@@ -16,5 +17,8 @@ public class SearchLyricsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search_lyrics);
 
         wvSearch = findViewById(R.id.wv_search);
+
+        wvSearch.setWebViewClient(new WebViewClient());
+        wvSearch.loadUrl("https://google.com");
     }
 }
